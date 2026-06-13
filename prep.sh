de54425c21b2a6f489f78c838f39568a57015f3b
lordpipon <126490646+lordpipon@users.noreply.github.com>
2026-06-13 21:39:14 +0200

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,6 +1,6 @@
 docker network create shared_backend
-docker run -d --name XprismPlay-redis --network shared_backend -p 6379:6379 -v XprismPlay_redisdata:/data --restart unless-stopped redis:8-alpine redis-server --save 60 1
-docker run -d --name XprismPlay-postgres --network shared_backend -e POSTGRES_USER=pguser -e POSTGRES_PASSWORD=pgpass -e POSTGRES_DB=XprismPlay -p 5432:5432 -v XprismPlay_pgdata:/var/lib/postgresql/data --restart unless-stopped pgvector/pgvector:pg16
+docker run -d --name dingoplay-redis --network shared_backend -p 6379:6379 -v dingoplay_redisdata:/data --restart unless-stopped redis:8-alpine redis-server --save 60 1
+docker run -d --name dingoplay-postgres --network shared_backend -e POSTGRES_USER=dingouser -e POSTGRES_PASSWORD=urpass -e POSTGRES_DB=dingoplay -p 5432:5432 -v dingoplay_pgdata:/var/lib/postgresql/data --restart unless-stopped pgvector/pgvector:pg16
 cd website
 cp .env.example .env
 cd ..
